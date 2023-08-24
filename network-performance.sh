85fd96e487cb38617159911699df672f046904d4
hgain <43654524+hgain@users.noreply.github.com>
2023-08-24 22:52:11 +0200

    variables renamed

--- network-performance.sh
+++ network-performance.sh
@@ -2,24 +2,23 @@ clear
 # Get the directory of the currently executing script
 script_dir=$(dirname "$0")
 
-# Get the name of the currently executing script without the extension
-script_name=$(basename "$0" .sh)
-
 # Get the current timestamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Formulate the output filename
-output_filename="${script_name}_${timestamp}.txt"
+output_filename="${timestamp}.txt"
 
 # Start capturing the output and saving it in the same directory as the script
 {
   echo "TESTING LOCAL NETWORK QUALITY -> PERFORMANCE TO DIRECTLY CONNECTED NEAR-END DEVICE"
   networkquality
 
-  echo "\nTESTING LOCAL NETWORK PACKAGE EXCHANGE SPEED -> PERFORMANCE TO NEAR-END MASTER ROUTER"
-  getIP=$(route -n get www.google.com | grep gateway)
-  ip=$(echo "$getIP" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
-  ping_stats=$(ping $ip -s 1024 -i 0.25 -c 50 --apple-time | sed -n "/--- $ip ping statistics ---/,/^---/p")
+  echo "\nTESTING LOCAL NETWORK PACKAGE EXCHANGE SPEED -> PERFORMANCE TO NEAR-END GATEWAY ROUTER"
+  getip_gateway=$(route -n get www.google.com | grep gateway)
+  ip_gateway=$(echo "$getip_gateway" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
+  hostname=$(arp $ip_gateway)
+  echo "Hostname: '$hostname'"
+  ping_stats=$(ping $ip_gateway -s 1024 -i 0.25 -c 50 --apple-time | sed -n "/--- $ip_gateway ping statistics ---/,/^---/p")
   echo "$ping_stats"
 
   echo "\nTESTING INTERNET SPEED -> PERFORMANCE TO REMOTE-ENDPOINT"
