bc41f51fc41e6813cfeab7a36002845e7f684c6a
hgain <43654524+hgain@users.noreply.github.com>
2023-08-24 21:38:27 +0200

    changed step heading echos

--- network-performance.sh
+++ network-performance.sh
@@ -13,16 +13,16 @@ output_filename="${script_name}_${timestamp}.txt"
 
 # Start capturing the output and saving it in the same directory as the script
 {
-  echo "starting nearest router connection performance test . . ."
+  echo "TESTING LOCAL NETWORK QUALITY -> CLOSEST NEAR-END ROUTER"
   networkquality
 
-  echo "\nstarting local master router package performance test . . ."
+  echo "\nTESTING LOCAL NETWORK PACKAGE EXCHANGE SPEED -> NEAR-END MASTER ROUTER"
   getIP=$(route -n get www.google.com | grep gateway)
   ip=$(echo "$getIP" | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
   ping_stats=$(ping $ip -s 1024 -i 0.25 -c 50 --apple-time | sed -n "/--- $ip ping statistics ---/,/^---/p")
   echo "$ping_stats"
 
-  echo "\nstarting remote endpoint up+downstream performance test . . ."
+  echo "\nTESTING INTERNET SPEED -> REMOTE-ENDPOINT"
   #speedtest-cli --list
   speedtest-cli
 } 2>&1 | sed -e 's/\x1B\[[0-9;]*[JKmsu]//g' | tee "${script_dir}/${output_filename}"
